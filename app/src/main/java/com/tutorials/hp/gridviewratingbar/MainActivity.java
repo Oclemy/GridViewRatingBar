37d6e67e494210071faffd81e022133a49e340da
Oclemy <oclemmi@gmail.com>
2017-01-22 00:49:02 -0800

    Initial Commit

---
+++ app/src/main/java/com/tutorials/hp/gridviewratingbar/MainActivity.java
@@ -0,0 +1,39 @@
+package com.tutorials.hp.gridviewratingbar;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+
+import com.tutorials.hp.gridviewratingbar.mAdapterView.CustomAdapter;
+import com.tutorials.hp.gridviewratingbar.mData.SpaceshipsCollection;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        GridView gv= (GridView) findViewById(R.id.gv);
+        gv.setAdapter(new CustomAdapter(this,SpaceshipsCollection.getSpaceships()));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
